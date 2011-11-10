b272fd83fb00ef1d875b2d67dcf86203d792c18b
Aaron Ecay <aaronecay@gmail.com>
2011-11-09 21:38:17 -0500

    add coindexing to addSister fn
    a stopgap until a real coindex fn is implemented

--- treetrans/transformer.py
+++ treetrans/transformer.py
@@ -1,10 +1,43 @@
 import nltk.tree as T
 
+def parseTraces(tree):
+    # TODO: this must be made to understand the new format, where I
+    # think I want indices in METADATA
+    def getTrace(tree):
+        temp = tree.node.split("-")
+        temp.pop()          # remove the lemma
+        temp2 = 0
+        try:
+            temp2 = int(temp[-1])
+        except ValueError:
+            # last element is not a parseable number
+            pass
+        except IndexError:
+            # the list had only 1 item, and is now empty
+            pass
+        return temp2
+    return reduce(max, map(getTrace, tree.subtrees()))
+
+def addIndexToTree(index, tree):
+    # TODO: do dash-tags go inside or outside index?
+    if isinstance(tree[0], str) and tree[0][0] == "*":
+        # This is an EC; so add index to the leaf
+        temp = tree[0].split("-")
+        if len(temp) == 1:
+            temp.append(str(index))
+        else:
+            temp.insert(-1, str(index))
+        tree[0] = "-".join(temp)
+    else:
+        # not EC; add index to the label
+        tree.node = tree.node + "-" + str(index)
+
 class TreeTransformer:
     def __init__(self, tree):
         # Do not mutate the tree we are given -- make a copy for our use.
         self._tree = tree.copy(True)
         self._matches = []
+        self._max_trace = parseTraces(self._tree)
 
     # remove me?
     def _testPos(self, p, fn):
@@ -127,15 +160,24 @@ class TreeTransformer:
                 m.node = label
         return self
 
-    def addSister(self, label = "XXX", word = "X-X", tree = None, before = True):
+    def addSister(self, label = "XXX", word = "X-X", tree = None, before = True,
+                  coindex = False):
         for m in self._matches:
             p = m.parent
             pi = m.parent_index
             to_insert = tree or T.ParentedTree(label, [word])
+            if coindex:
+                index = self._max_trace + 1
+                # TODO(?): a way to add gapping indices with = instead of -
+                addIndexToTree(index, m)
+                addIndexToTree(index, to_insert)
+                self._max_trace = index
             if not before:
                 pi = pi + 1
             p.insert(pi, to_insert)
         return self
+
+# TODO: add a coindex function, operating on a dual selection
 
     def prune(self):
         for m in self._matches:
