9ed7087467ac6197cd0ffbf35fcc044ad87b99f9
Aaron Ecay <aaronecay@gmail.com>
2012-05-23 00:55:34 -0400

    annotald: new version of flagif for new convention

--- lovett/annotald.py
+++ lovett/annotald.py
@@ -2,13 +2,26 @@ import sys
 import transformer as TT
 import nltk.tree as T
 
+# def flagIf(expr):
+#     data = sys.stdin.read()
+#     data = data.replace("-FLAG", "") # TODO: do this in a smarter way
+#     trees = data.split("\n\n")
+#     for tree in trees:
+#         trans = TT.TreeTransformer(T.ParentedTree(tree))
+#         trans.findNodes(expr)
+#         trans.changeLabel(lambda x: x + "-FLAG")
+#         print trans.pt() + "\n\n"
+#     sys.stdout.close()
+
 def flagIf(expr):
-    data = sys.stdin.read()
-    data = data.replace("-FLAG", "") # TODO: do this in a smarter way
-    trees = data.split("\n\n")
-    for tree in trees:
-        trans = TT.TreeTransformer(T.ParentedTree(tree))
-        trans.findNodes(expr)
-        trans.changeLabel(lambda x: x + "-FLAG")
-        print trans.pt() + "\n\n"
-    sys.stdout.close()
+    def _flagIfInner(version, trees):
+        trees = trees.replace("-FLAG", "")
+        trees = trees.split("\n\n")
+        results = []
+        for tree in trees:
+            trans = TT.TreeTransformer(T.ParentedTree(tree))
+            trans.findNodes(expr)
+            trans.changeLabel(lambda x: x + "-FLAG")
+            results.append(trans.pt())
+        return '\n\n'.join(results)
+    return _flagIfInner
