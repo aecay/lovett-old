25f4a2b202df05f0717b94c3f1dd4fae7fe28403
Aaron Ecay <aaronecay@gmail.com>
2012-05-25 17:34:39 -0400

    annotald: add stdinValidator function

--- lovett/annotald.py
+++ lovett/annotald.py
@@ -1,17 +1,7 @@
 import sys
 import transformer as TT
 import nltk.tree as T
-
-# def flagIf(expr):
-#     data = sys.stdin.read()
-#     data = data.replace("-FLAG", "") # TODO: do this in a smarter way
-#     trees = data.split("\n\n")
-#     for tree in trees:
-#         trans = TT.TreeTransformer(T.ParentedTree(tree))
-#         trans.findNodes(expr)
-#         trans.changeLabel(lambda x: x + "-FLAG")
-#         print trans.pt() + "\n\n"
-#     sys.stdout.close()
+import subprocess
 
 def flagIf(expr):
     def _flagIfInner(version, trees):
@@ -25,3 +15,15 @@ def flagIf(expr):
             results.append(trans.pt())
         return '\n\n'.join(results)
     return _flagIfInner
+
+
+def stdinValidator(path):
+    def _stdinValidator(version, trees):
+        print "path is: " + path
+        proc = subprocess.Popen(path,
+                                stdin = subprocess.PIPE,
+                                stdout = subprocess.PIPE)
+        out = proc.communicate(input = trees.encode('utf-8'))[0]
+        print "out is: " + out.decode('utf-8')
+        return out.decode('utf-8')
+    return _stdinValidator
