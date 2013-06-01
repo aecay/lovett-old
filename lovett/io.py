bb81716c460115310474ff25b33e1db44d3beab5
Aaron Ecay <aaronecay@gmail.com>
2013-06-01 15:09:31 -0400

    lovett/io.py: updates

--- lovett/io.py
+++ lovett/io.py
@@ -1,11 +1,11 @@
 from __future__ import unicode_literals
 
-import lovett.tree, lovett.corpus
-
-import sys
-import multiprocessing
+import lovett.tree
+import lovett.corpus
+import lovett.util
 
 __docformat__ = "restructuredtext en"
+
 
 def _stripComments(lines):
     r = []
@@ -28,36 +28,10 @@ def _stripComments(lines):
         raise Exception("unbalanced comment")
     return r
 
-def strip(s):
-    return s.strip()
+
 def parse(s):
     return lovett.tree.LovettTree.parse(s)
 
-def readTrees(f, stripComments = False, parallel = True):
-    """Read trees from a file.
-
-    :param f: the file to read from
-    :type f: file
-
-    """
-    current_tree = ""
-    comment = False
-    all_trees = []
-    lines = f.readlines()
-    if stripComments:
-        lines = _stripComments(lines)
-    trees = "".join(lines).split("\n\n")
-    sys.stderr.write("parallel is: %s\n" % parallel)
-    if parallel:
-        p = multiprocessing.Pool()
-        trees = p.map(strip, trees)
-        trees = filter(lambda s: s != "", trees)
-        trees = p.map(parse, trees)
-    else:
-        trees = map(lambda s: s.strip(), trees)
-        trees = filter(lambda s: s != "", trees)
-        trees = map(lovett.tree.LovettTree.parse, trees)
-    return trees
 
 def writeTrees(metadata, trees, file):
     # TODO: if trees are nltk trees (not strings), this barfs
@@ -69,7 +43,7 @@ def writeTrees(metadata, trees, file):
 
     """
     if metadata:
-        file.write(_dictToMetadata(metadata) + "\n\n")
+        file.write(lovett.util._dictToMetadata(metadata) + "\n\n")
     if isinstance(trees[0], lovett.tree.LovettTree):
-        trees = map(unicode, trees)
-    file.write("\n\n".join(trees))
+        trees = map(str, trees)
+        file.write("\n\n".join(trees))
