1429005a25593d859de3c97ce42588d22526d582
Aaron Ecay <aaronecay@gmail.com>
2013-06-01 15:08:14 -0400

    tests/util_test.py: convert to tree_new

--- tests/util_test.py
+++ tests/util_test.py
@@ -2,27 +2,26 @@ import unittest
 from io import StringIO
 import textwrap
 
-from lovett.tree import LovettTree
-from lovett.corpus import Corpus
+import lovett.tree_new
 import lovett.util
 
 class UtilTest(unittest.TestCase):
     def test_treeToDict(self):
-        t = LovettTree.parse("((FOO bar) (BAZ (QUUX 1) (BLORFLE 2)))")
+        t = lovett.tree_new.parse("(METADATA (FOO bar) (BAZ (QUUX 1) (BLORFLE 2)))")
         self.assertEqual(lovett.util._treeToDict(t),
                          { "FOO" : "bar",
                            "BAZ" : { "QUUX" : "1",
                                      "BLORFLE" : "2"}})
 
     def test_parseVersionTree(self):
-        t = LovettTree.parse("""
+        t = lovett.tree_new.parse("""
                                    ( (VERSION (FORMAT dash)
                                               (SOMETHING else)))
                                    """)
         self.assertEqual(lovett.util._parseVersionTree(t),
                          { "FORMAT" : "dash",
                            "SOMETHING" : "else" })
-        t = LovettTree.parse("((FOO bar) (BAZ (QUUX 1) (BLORFLE 2)))")
+        t = lovett.tree_new.parse("( (FOO (FOO bar) (BAZ (QUUX 1) (BLORFLE 2))))")
         self.assertIsNone(lovett.util._parseVersionTree(t))
 
     # def test_dictToTrees(self):
