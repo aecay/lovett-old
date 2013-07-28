5646d1e1cb6dfe862fb8d8c3d00f548480cd9c77
Aaron Ecay <aaronecay@gmail.com>
2013-07-28 16:43:44 -0400

    setup.py: lowercase name

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name = "Lovett",
+    name = "lovett",
     author = "Aaron Ecay",
     author_email = "aaronecay@gmail.com",
     version = "0.1dev",
     packages = ["lovett","lovett.cs"],
+    provides = ["lovett"],
     license = "GNU General Public License v.3 or higher"#,
     # long_description = open("README").read()
     # scripts = []
