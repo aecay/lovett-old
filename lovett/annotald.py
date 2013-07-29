1a3644311d6b52defc6eb23b32d90baddaf66ac1
Aaron Ecay <aaronecay@gmail.com>
2013-07-29 00:42:51 -0400

    annotald.py: get flagIf brushed up some

--- lovett/annotald.py
+++ lovett/annotald.py
@@ -1,30 +1,23 @@
 from __future__ import unicode_literals
-import sys
+
 import lovett.cs.transformer as TT
-import lovett.tree_new as T
 import lovett.corpus
-import subprocess
-import itertools
-import lovett.cs.searchfns
+
 
 def _flagIfHelper(tree, expr):
     trans = TT.TreeTransformer(tree)
-    trans.findNodes(lovett.cs.searchfns.isLeaf())
-    # for m in trans.matches():
-    #     print(m.metadata)
     trans.findNodes(expr)
-    trans.changeLabel(lambda x: x + "-FLAG")
-    return trans._tree
+    if len(trans.matches()) > 0:
+        trans.changeLabel(lambda x: x + "-FLAG")
+    return trans.tree()
 
 def flagIf(expr):
     def _flagIfInner(version, trees):
-        print(version)
         trees = trees.replace("-FLAG", "")
         trees = trees.split("\n\n")
-        corpus = lovett.corpus.Corpus.fromTreeStrings(trees, version)
-        corpus._mapTrees(_flagIfHelper, itertools.repeat(expr))
-        print(str(corpus.trees[2]))
-        r = '\n\n'.join(map(str, corpus.trees))
+        c = lovett.corpus.Corpus.fromTreeStrings(trees, version)
+        c._mapTrees(lambda x: _flagIfHelper(x, expr))
+        r = '\n\n'.join(map(str, c.trees))
         return r
     return _flagIfInner
 
