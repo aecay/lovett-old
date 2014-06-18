2ce747af3f903e6d75a8d299ecd2539b4ca9dcf1
Aaron Ecay <aaronecay@gmail.com>
2014-06-17 20:26:32 -0400

    update tests

--- lovett/tests/corpus_test.py
+++ lovett/tests/corpus_test.py
@@ -1,6 +1,8 @@
 import testtools
-from ..corpus import (parse_string, _validate_psdx)
+from testtools.matchers import (Not, IsInstance)
+from ..corpus import (parse_string, _validate_psdx, Corpus)
 from .utils import PassesValidation
+from ..tree import (Sentence, NonTerminal, Text, Trace, Ec, Comment)
 
 def T(string):
     return _validate_psdx(parse_string(string))
@@ -18,3 +20,28 @@ class TestCorpusXml(testtools.TestCase):
             </corpus>
             """
         ), PassesValidation())
+
+    def test_validator_fail(self):
+        self.assertThat(T(
+            """
+            <corpus>
+            <sentence d="XYZ">
+            <text category="FOO">
+            bar
+            </text>
+            </sentence>
+            </corpus>
+            """
+        ), Not(PassesValidation()))
+
+    def test_parser(self):
+        cases = (("corpus", Corpus),
+                 ("sentence", Sentence),
+                 ("nonterminal", NonTerminal),
+                 ("trace", Trace),
+                 ("ec", Ec),
+                 ("text", Text),
+                 ("comment", Comment))
+        for case in cases:
+            parsed = parse_string("<%s/>" % case[0])
+            self.expectThat(parsed, IsInstance(case[1]))
--- lovett/tests/tree_test.py
+++ lovett/tests/tree_test.py
@@ -3,7 +3,7 @@ import lxml.etree
 
 from lovett.corpus import parse_string
 
-class TreeXmlTest(testtools.TestCase):
+class TreeTest(testtools.TestCase):
     def test_eq(self):
         one = parse_string("<text category=\",\">,</text>")
         two = parse_string("<text category=\",\">,</text>")
@@ -28,3 +28,48 @@ class TreeXmlTest(testtools.TestCase):
         one = parse_string("<text category=\",\"><meta/>,</text>")
         two = parse_string("<text category=\",\">,</text>")
         self.assertEqual(one, two)
+
+    # TreeNode
+    def test_label(self):
+        self.skipTest("unwritten test")
+
+    def test_set_label(self):
+        self.skipTest("unwritten test")
+
+    def test_urtext(self):
+        self.skipTest("unwritten test")
+
+    def test_sentence_node(self):
+        self.skipTest("unwritten test")
+
+    def test_left_sibling(self):
+        self.skipTest("unwritten test")
+
+    def test_right_sibling(self):
+        self.skipTest("unwritten test")
+
+    def test_iter(self):
+        self.skipTest("unwritten test")
+
+    # MetadataDict
+
+    def test_illegal_attrs(self):
+        self.skipTest("unwritten test")
+
+    # NonTerminal
+
+    def test_children(self):
+        self.skipTest("unwritten test")
+
+    def test_subtrees(self):
+        self.skipTest("unwritten test")
+
+    # Sentence
+    def test_tree(self):
+        self.skipTest("unwritten test")
+
+    def test_id(self):
+        self.skipTest("unwritten test")
+
+    def test_set_id(self):
+        self.skipTest("unwritten test")
